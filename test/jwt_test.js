23964bfbae4f17eaa2b3c4ad571ba707ff68dde2
Blain Smith <rebelgeek@blainsmith.com>
2015-12-15 15:58:16 -0500

    test string generation and new key arg

--- test/jwt_test.js
+++ test/jwt_test.js
@@ -10,20 +10,25 @@ var payload = {
 	github: 'blainsmith'
 };
 
-var secret = 'shhhhh';
-var hs256token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoiQmxhaW4gU21pdGgiLCJlbWFpbCI6InJlYmVsZ2Vla0BibGFpbnNtaXRoLmNvbSIsImdpdGh1YiI6ImJsYWluc21pdGgifQ.AklQQAaB8EF4ryY35mL87hXRoPiM5O3ShiSAYES-RZo';
+test('Generate a String Key', function (t) {
+	t.plan(1);
 
-var privateKey = fs.readFileSync(path.join(__dirname, '/keys/jwt'));
-var publicKey = fs.readFileSync(path.join(__dirname, '/keys/jwt.pub'));
-var rs256token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJuYW1lIjoiQmxhaW4gU21pdGgiLCJlbWFpbCI6InJlYmVsZ2Vla0BibGFpbnNtaXRoLmNvbSIsImdpdGh1YiI6ImJsYWluc21pdGgifQ.Mj6OOD4ObDlefdgO0Raw-Y7fzoAuy6ag4p9_CcIA1FUwcYJCWoAXBPJSQIU5nRC6WGK934-i83Un7TO9s3XkubvH4ztta1uoIpv56kRHP9aZhIVZqLmMXNOGakzxt2lpbXpoFUUsSbEUyDzg3wTf2dcH88dv87QzCL28YPCdjcUHQyvjQ11mU-A3DolW1llSE_n_QjRks6alDkis88MmRh6Rj8YnNTo629wBlOeNUM-OgaQJWwro817SDPc5x36g65fBkoxoJIgBnZSpPRDy4fVNT3_uifSYmh9z4LB_lDIFl_U-pl0H-tfU1N1kt5jLOn69DIYg0hyjpnkwU5H8LQ';
+	seneca.act({role: 'jwt', cmd: 'generateKey'}, function (errGenKey, resultGenKey) {
+		t.ok(/^[a-zA-Z0-9+/]+={0,2}$/i.test(resultGenKey.key), 'generates a base64-encoded string');
+		t.end(errGenKey);
+	});
+});
 
-test('Secret String', function (t) {
+test('Secret String Key', function (t) {
 	t.plan(3);
 
-	seneca.act({role: 'jwt', cmd: 'sign', payload: payload, secret: secret}, function (errSign, resultSign) {
+	var key = 'shhhhh';
+	var hs256token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoiQmxhaW4gU21pdGgiLCJlbWFpbCI6InJlYmVsZ2Vla0BibGFpbnNtaXRoLmNvbSIsImdpdGh1YiI6ImJsYWluc21pdGgifQ.AklQQAaB8EF4ryY35mL87hXRoPiM5O3ShiSAYES-RZo';
+
+	seneca.act({role: 'jwt', cmd: 'sign', payload: payload, key: key}, function (errSign, resultSign) {
 		t.equal(resultSign.token, hs256token, 'signing yields correct token');
 
-		seneca.act({role: 'jwt', cmd: 'verify', token: resultSign.token, secret: secret}, function (errVerify, resultVerify) {
+		seneca.act({role: 'jwt', cmd: 'verify', token: resultSign.token, key: key}, function (errVerify, resultVerify) {
 			t.deepEqual(resultVerify, payload, 'verifying yields the correct payload');
 
 			seneca.act({role: 'jwt', cmd: 'decode', token: resultSign.token}, function (errDecode, resultDecode) {
@@ -36,10 +41,14 @@ test('Secret String', function (t) {
 test('Public/Private Keys', function (t) {
 	t.plan(3);
 
-	seneca.act({role: 'jwt', cmd: 'sign', payload: payload, secret: privateKey, algorithm: 'RS256'}, function (errSign, resultSign) {
+	var privateKey = fs.readFileSync(path.join(__dirname, '/keys/jwt'));
+	var publicKey = fs.readFileSync(path.join(__dirname, '/keys/jwt.pub'));
+	var rs256token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJuYW1lIjoiQmxhaW4gU21pdGgiLCJlbWFpbCI6InJlYmVsZ2Vla0BibGFpbnNtaXRoLmNvbSIsImdpdGh1YiI6ImJsYWluc21pdGgifQ.Mj6OOD4ObDlefdgO0Raw-Y7fzoAuy6ag4p9_CcIA1FUwcYJCWoAXBPJSQIU5nRC6WGK934-i83Un7TO9s3XkubvH4ztta1uoIpv56kRHP9aZhIVZqLmMXNOGakzxt2lpbXpoFUUsSbEUyDzg3wTf2dcH88dv87QzCL28YPCdjcUHQyvjQ11mU-A3DolW1llSE_n_QjRks6alDkis88MmRh6Rj8YnNTo629wBlOeNUM-OgaQJWwro817SDPc5x36g65fBkoxoJIgBnZSpPRDy4fVNT3_uifSYmh9z4LB_lDIFl_U-pl0H-tfU1N1kt5jLOn69DIYg0hyjpnkwU5H8LQ';
+
+	seneca.act({role: 'jwt', cmd: 'sign', payload: payload, key: privateKey, algorithm: 'RS256'}, function (errSign, resultSign) {
 		t.equal(resultSign.token, rs256token, 'signing yields correct token');
 
-		seneca.act({role: 'jwt', cmd: 'verify', token: resultSign.token, secret: publicKey}, function (errVerify, resultVerify) {
+		seneca.act({role: 'jwt', cmd: 'verify', token: resultSign.token, key: publicKey}, function (errVerify, resultVerify) {
 			t.deepEqual(resultVerify, payload, 'verifying yields the correct payload');
 
 			seneca.act({role: 'jwt', cmd: 'decode', token: resultSign.token}, function (errDecode, resultDecode) {
