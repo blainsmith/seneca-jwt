3f8008a2887954bb9c71bbc999b1cea52f165e8a
Blain Smith <rebelgeek@blainsmith.com>
2015-12-15 22:04:43 -0500

    error handling tests

--- test/jwt_test.js
+++ test/jwt_test.js
@@ -1,7 +1,7 @@
 var path = require('path');
 var fs = require('fs');
 var test = require('tape');
-var seneca = require('seneca')();
+var seneca = require('seneca')(require('./config'));
 seneca.use('..');
 
 var payload = {
@@ -9,6 +9,7 @@ var payload = {
 	email: 'rebelgeek@blainsmith.com',
 	github: 'blainsmith'
 };
+var hs256token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoiQmxhaW4gU21pdGgiLCJlbWFpbCI6InJlYmVsZ2Vla0BibGFpbnNtaXRoLmNvbSIsImdpdGh1YiI6ImJsYWluc21pdGgifQ.AklQQAaB8EF4ryY35mL87hXRoPiM5O3ShiSAYES-RZo';
 
 test('Generate a String Key', function (t) {
 	t.plan(1);
@@ -23,7 +24,6 @@ test('Secret String Key', function (t) {
 	t.plan(3);
 
 	var key = 'shhhhh';
-	var hs256token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoiQmxhaW4gU21pdGgiLCJlbWFpbCI6InJlYmVsZ2Vla0BibGFpbnNtaXRoLmNvbSIsImdpdGh1YiI6ImJsYWluc21pdGgifQ.AklQQAaB8EF4ryY35mL87hXRoPiM5O3ShiSAYES-RZo';
 
 	seneca.act({role: 'jwt', cmd: 'sign', payload: payload, key: key}, function (errSign, resultSign) {
 		t.equal(resultSign.token, hs256token, 'signing yields correct token');
@@ -54,6 +54,20 @@ test('Public/Private Keys', function (t) {
 			seneca.act({role: 'jwt', cmd: 'decode', token: resultSign.token}, function (errDecode, resultDecode) {
 				t.deepEqual(resultDecode, payload, 'decoding yields the correct payload');
 			});
+		});
+	});
+});
+
+test('Error Handling', function (t) {
+	t.plan(4);
+
+	seneca.act({role: 'jwt', cmd: 'sign', payload: payload}, function (errSign, resultSign) {
+		t.notOk(resultSign, 'no result returned');
+		t.equal(errSign.details.message, 'Unable to sign payload without a key', 'cannot sign without a key');
+
+		seneca.act({role: 'jwt', cmd: 'verify', token: hs256token}, function (errVerify, resultVerify) {
+			t.notOk(resultVerify, 'no result returned');
+			t.equal(errVerify.details.message, 'secret or public key must be provided', 'cannot verify without a key');
 		});
 	});
 });
